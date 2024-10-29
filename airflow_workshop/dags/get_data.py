86b61dc29c14d8352112364a34a2d35ae35af976
kridaniil <kridaniil@yandex-team.ru>
2024-10-29 19:57:41 +0300

    STAGE LAYER: [fix] Move endpoint scripts to one file, fix script failure, add SCD2.

--- airflow_workshop/dags/get_data.py
+++ airflow_workshop/dags/get_data.py
@@ -16,31 +16,7 @@ token_txt = requests.post(url, auth_data).text
 token = json.loads(token_txt)["auth_token"]
 headers = {'Content-Type': "application/json", 'Authorization': "Token " + token}
 
-
-def get_wp_descriptions():
-    # нет учета времени, просто удаляем все записи
-    PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').run(
-    """
-    truncate stg.work_programs  restart identity cascade;
-    """)
-    target_fields = ['id', 'academic_plan_in_field_of_study', 'wp_in_academic_plan', 'update_ts']
-    url_down = 'https://op.itmo.ru/api/record/academic_plan/academic_wp_description/all?format=json&page=1'
-    page = requests.get(url_down, headers=headers)
-    c = json.loads(page.text)['count']
-    for p in range(1,c//10+2):
-        url_down = 'https://op.itmo.ru/api/record/academic_plan/academic_wp_description/all?format=json&page=' + str(p)
-        dt = pendulum.now().to_iso8601_string()
-        page = requests.get(url_down, headers=headers)
-        res = json.loads(page.text)['results']
-        for r in res:
-            df = pd.DataFrame([r], columns=r.keys())
-            df['academic_plan_in_field_of_study'] = df[~df['academic_plan_in_field_of_study'].isna()]["academic_plan_in_field_of_study"].apply(lambda st_dict: json.dumps(st_dict))
-            df['wp_in_academic_plan'] = df[~df['wp_in_academic_plan'].isna()]["wp_in_academic_plan"].apply(lambda st_dict: json.dumps(st_dict))
-            df.loc[:, 'update_ts'] = dt
-            PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.work_programs', df.values, target_fields = target_fields)
-
 def get_practice():
-    # нет учета времени, просто удаляем все записи
     PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').run(
     """
     truncate stg.practice  restart identity cascade;
@@ -59,6 +35,39 @@ def get_practice():
             # df['wp_in_academic_plan'] = df[~df['wp_in_academic_plan'].isna()]["wp_in_academic_plan"].apply(lambda st_dict: json.dumps(st_dict))
             # df.loc[:, 'update_ts'] = dt
             PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.practice', df.values, target_fields = df.columns.tolist())
+
+def get_wp_descriptions():
+    # нет учета времени, просто удаляем все записи
+    PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').run(
+    """
+    truncate stg.work_programs  restart identity cascade;
+    """)
+    # target_fields = ['id', 'academic_plan_in_field_of_study', 'wp_in_academic_plan']
+    url_down = 'https://op.itmo.ru/api/record/academic_plan/academic_wp_description/all?format=json&page=1'
+    page = requests.get(url_down, headers=headers)
+    c = json.loads(page.text)['count']
+    for p in range(1,c//10+2):
+        url_down = 'https://op.itmo.ru/api/record/academic_plan/academic_wp_description/all?format=json&page=' + str(p)
+        # dt = pendulum.now().to_iso8601_string()
+        page = requests.get(url_down, headers=headers)
+        if page.status_code != 200:
+            print(f"Ошибка при получении данных с страницы {p}: {page.status_code}")
+            continue
+        try:
+            data = page.json()  # Использовать page.json(), это более надежный способ работы с JSON
+            if 'results' not in data:
+                print(f"Нет данных на странице {p}.")
+                continue
+            res = data['results']
+            for r in res:
+                df = pd.DataFrame([r], columns=r.keys())
+                # df['academic_plan_in_field_of_study'] = df[~df['academic_plan_in_field_of_study'].isna()]["academic_plan_in_field_of_study"].apply(lambda st_dict: json.dumps(st_dict))
+                # df['wp_in_academic_plan'] = df[~df['wp_in_academic_plan'].isna()]["wp_in_academic_plan"].apply(lambda st_dict: json.dumps(st_dict))
+                PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.work_programs', df.values, target_fields = df.columns.tolist())
+        except ValueError:
+            print(f"Ошибка JSON на странице {p}.")
+            continue
+        
 
 def get_structural_units():
     # нет учета времени, просто удаляем все записи
@@ -98,22 +107,133 @@ def get_online_courses():
             df['topic_with_online_course'] = df[~df['topic_with_online_course'].isna()]["topic_with_online_course"].apply(lambda st_dict: json.dumps(st_dict))
             PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.online_courses', df.values, target_fields = target_fields)
 
+def get_disc_by_year():
+    ids = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_records(
+    """
+    with t as (
+    select 
+    (json_array_elements(academic_plan_in_field_of_study::json)->>'ap_isu_id') :: integer as isu_id,
+    id
+    from stg.work_programs wp)
+    select id from t
+    where isu_id in
+    (
+    select id from stg.up_description ud 
+    where ((training_period = '2') and (selection_year > '2020'))
+    or ((training_period = '4') and (selection_year > '2018'))
+    ) and
+    id < 7256
+    order by id
+    """)
+    for up_id in ids:
+        up_id = str(up_id[0])
+        print (up_id)
+        url = 'https://op.itmo.ru/api/record/academicplan/get_wp_by_year/' + up_id + '?year=2022/2023'
+        page = requests.get(url, headers=headers)
+        df = pd.DataFrame.from_dict(page.json())
+        df['work_programs'] = df[~df['work_programs'].isna()]["work_programs"].apply(lambda st_dict: json.dumps(st_dict))
+        if len(df)>0:
+            PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.disc_by_year', df.values, target_fields=df.columns.tolist())
+
+def get_up_detail():
+    PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').run(
+    """
+    truncate stg.up_detail  restart identity cascade;
+    """)
+    ids = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_records(
+    """
+    select id as op_id
+    from stg.work_programs wp
+    where id > 7290
+    order by 1
+    """)
+    url_down = 'https://op.itmo.ru/api/academicplan/detail/'
+    target_fields = ['id', 'ap_isu_id', 'on_check', 'laboriousness', 'academic_plan_in_field_of_study']
+    for op_id in ids:
+        op_id = str(op_id[0])
+        print (op_id)
+        url = url_down + op_id + '?format=json'
+        page = requests.get(url, headers=headers)
+        df = pd.DataFrame.from_dict(page.json(), orient='index')
+        df = df.T
+        df['academic_plan_in_field_of_study'] = df[~df['academic_plan_in_field_of_study'].isna()]["academic_plan_in_field_of_study"].apply(lambda st_dict: json.dumps(st_dict))
+        df = df[target_fields]
+        PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.up_detail', df.values, target_fields = target_fields)
+
+#     page = requests.get(url_down, headers=headers)
+#     res = list(json.loads(page.text))
+#     for su in res:
+#         df = pd.DataFrame.from_dict(su)
+#         # превращаем последний столбец в json
+#         df['academic_plan_in_field_of_study'] = df[~df['academic_plan_in_field_of_study'].isna()]["academic_plan_in_field_of_study"].apply(lambda st_dict: json.dumps(st_dict))
+#         PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.up_detail', df.values, target_fields = target_fields)
+
+# url_down = 'https://op.itmo.ru/api/workprogram/items_isu/'
+#     for wp_id in ids:
+#         wp_id = str(wp_id[0])
+#         print (wp_id)
+#         url = url_down + wp_id + '?format=json'
+#         page = requests.get(url, headers=headers)
+#         df = pd.DataFrame.from_dict(page.json(), orient='index')
+#         df = df.T
+#         df['prerequisites'] = df[~df['prerequisites'].isna()]["prerequisites"].apply(lambda st_dict: json.dumps(st_dict))
+#         df['outcomes'] = df[~df['outcomes'].isna()]["outcomes"].apply(lambda st_dict: json.dumps(st_dict))
+#         if len(df)>0:
+#             PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.wp_markup', df.values, target_fields=df.columns.tolist(), replace=True, replace_index='id')
+
+# "id": 6796,
+# "ap_isu_id": 10572,
+# "on_check": "in_work",
+# "laboriousness": 393,
+# "academic_plan_in_field_of_study": [
+#         {
+#             "id": 6859,
+#             "year": 2018,
+#             "qualification": "bachelor",
+#             "title": "Нанофотоника и квантовая оптика",
+#             "field_of_study": [
+#                 {
+#                     "number": "16.03.01",
+#                     "id": 15772,
+#                     "title": "Техническая физика",
+#                     "qualification": "bachelor",
+#                     "educational_profile": null,
+#                     "faculty": null
+#                 }
+#             ],
+#             "plan_type": "base",
+#             "training_period": 0,
+#             "structural_unit": null,
+#             "total_intensity": 0,
+#             "military_department": false,
+#             "university_partner": [],
+#             "editors": []
+#         }
+
 with DAG(dag_id='get_data', start_date=pendulum.datetime(2022, 1, 1, tz="UTC"), schedule_interval='0 1 * * *', catchup=False) as dag:
-    t1 = PythonOperator(
-    task_id='get_practice',
-    python_callable=get_practice
-    )
+    # t1 = PythonOperator(
+    # task_id='get_practice',
+    # python_callable=get_practice
+    # )
     t2 = PythonOperator(
     task_id='get_wp_descriptions',
     python_callable=get_wp_descriptions
     )
-    t3 = PythonOperator(
-    task_id='get_structural_units',
-    python_callable=get_structural_units
-    )
-    t4 = PythonOperator(
-    task_id='get_online_courses',
-    python_callable=get_online_courses
-    )
-
-t1 >> t2 >> t3 >> t4
+    # t3 = PythonOperator(
+    # task_id='get_structural_units',
+    # python_callable=get_structural_units
+    # )
+    # t4 = PythonOperator(
+    # task_id='get_online_courses',
+    # python_callable=get_online_courses
+    # )
+    # t5 = PythonOperator(
+    # task_id='get_disc_by_year',
+    # python_callable=get_disc_by_year
+    # )
+    # t6 = PythonOperator(
+    # task_id='get_up_detail',
+    # python_callable=get_up_detail
+    # )
+t2
+# t1 >> t2 >> t3 >> t4 >> t5 >> t6
--- airflow_workshop/sql/stg_ddl.sql
+++ airflow_workshop/sql/stg_ddl.sql
@@ -1,33 +1,229 @@
-create table stg.work_programs
-(id integer, academic_plan_in_field_of_study text, wp_in_academic_plan text, update_ts timestamp);
-create table stg.up_description
-(id integer, plan_type text, direction_id text, ns_id text, direction_code text, direction_name text, edu_program_id text, edu_program_name text, 
-faculty_id text, faculty_name text, training_period text, university_partner text, up_country text, lang text, military_department boolean, total_intensity text, 
-ognp_id text, ognp_name text, selection_year text);
+-- Таблицы
+
+create table stg.work_programs (
+    id integer, 
+    academic_plan_in_field_of_study text, 
+    wp_in_academic_plan text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
+
+create table stg.up_description (
+    id integer,
+    plan_type text, 
+    direction_id text, 
+    ns_id text, 
+    direction_code text, 
+    direction_name text, 
+    edu_program_id text, 
+    edu_program_name text, 
+    faculty_id text, 
+    faculty_name text, 
+    training_period text, 
+    university_partner text, 
+    up_country text, 
+    lang text, 
+    military_department boolean, 
+    total_intensity text, 
+    ognp_id text, 
+    ognp_name text, 
+    selection_year text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
 ALTER TABLE stg.up_description ADD CONSTRAINT up_description_uindex UNIQUE (id);
-create table stg.su_wp
-(fak_id integer, fak_title text, wp_list text);
-create table stg.wp_markup
-(id integer, title text, discipline_code integer, prerequisites text, outcomes text);
+
+CREATE OR REPLACE FUNCTION scd2_up_description_trigger()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE stg.work_programs
+    SET valid_to = NOW(), is_current = FALSE
+    WHERE id = OLD.id AND is_current = TRUE;
+
+    INSERT INTO stg.work_programs (id, academic_plan_in_field_of_study, wp_in_academic_plan, valid_from, valid_to, is_current)
+    VALUES (NEW.id, NEW.academic_plan_in_field_of_study, NEW.wp_in_academic_plan, NOW(), '2999-12-31', TRUE);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_update_work_programs
+BEFORE UPDATE ON stg.work_programs
+FOR EACH ROW
+WHEN (OLD.* IS DISTINCT FROM NEW.*)
+EXECUTE FUNCTION scd2_work_programs_trigger();
+
+create table stg.su_wp (
+    fak_id integer, 
+    fak_title text, 
+    wp_list text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
+
+create table stg.wp_markup (
+    id integer, 
+    title text, 
+    discipline_code integer, 
+    prerequisites text, 
+    outcomes text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
 ALTER TABLE stg.wp_markup ADD CONSTRAINT wp_id_uindex UNIQUE (id);
-create table stg.online_courses
-(id integer, institution text, title text, topic_with_online_course text);
-create table stg.evaluation_tools
-(id integer, type text, "name" text, description text, check_point bool, deadline integer, semester integer, 
-"min" numeric, "max" numeric, descipline_sections text, evaluation_criteria text, wp_id integer);
+
+create table stg.online_courses (
+    id integer, 
+    institution text, 
+    title text, 
+    topic_with_online_course text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
+
+create table stg.evaluation_tools (
+    id integer, 
+    type text, 
+    "name" text, 
+    description text, 
+    check_point bool, 
+    deadline integer, 
+    semester integer, 
+    "min" numeric, 
+    "max" numeric, 
+    descipline_sections text,
+    evaluation_criteria text, 
+    wp_id integer,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
 ALTER TABLE stg.evaluation_tools ADD CONSTRAINT et_id_uindex UNIQUE (id);
-create table stg.disc_by_year
-(id integer, ap_isu_id integer, title text, work_programs text);
-create table stg.up_detail
-(id integer, ap_isu_id integer, on_check varchar(20), laboriousness integer, academic_plan_in_field_of_study text);
-create table stg.up_isu
-(id integer, plan_type text, direction_id text, ns_id text, direction_code text, direction_name text, edu_program_id text, edu_program_name text, 
-faculty_id text, faculty_name text, training_period text, university_partner text, up_country text, lang text, military_department boolean, total_intensity text, 
-ognp_id text, ognp_name text, selection_year text, disciplines_blocks text);
-create table stg.wp_detail
-(id integer, discipline_code varchar(20), title text, description text, structural_unit varchar(100), prerequisites text, discipline_sections text, bibliographic_reference text, outcomes text, certification_evaluation_tools text, expertise_status varchar(3));
-create table stg.practice
-(id integer, discipline_code varchar(20), title text, year text, authors text, op_leader text, language varchar(20), qualification text, kind_of_practice text,
-type_of_practice text, way_of_doing_practice text, format_practice text, features_content_and_internship text, features_internship text, additional_reporting_materials text, form_of_certification_tools text,
-passed_great_mark text, passed_good_mark text, passed_satisfactorily_mark text, not_passed_mark text, evaluation_tools_current_control text, prac_isu_id text, ze_v_sem varchar(30), 
-evaluation_tools_v_sem text, number_of_semesters text, practice_base varchar(20),  structural_unit varchar(30), editors text, bibliographic_reference text, prerequisites text, outcomes text);
+
+create table stg.disc_by_year (
+    id integer, 
+    ap_isu_id integer, 
+    title text, 
+    work_programs text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
+
+create table stg.up_detail (
+    id integer,
+    ap_isu_id integer, 
+    on_check varchar(20), 
+    laboriousness integer, 
+    academic_plan_in_field_of_study text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
+
+create table stg.up_isu (
+    id integer, 
+    plan_type text, 
+    direction_id text, 
+    ns_id text, 
+    direction_code text, 
+    direction_name text, 
+    edu_program_id text, 
+    edu_program_name text, 
+    faculty_id text, 
+    faculty_name text,
+    training_period text,
+    university_partner text,
+    up_country text, 
+    lang text, 
+    military_department boolean,
+    total_intensity text, 
+    ognp_id text, 
+    ognp_name text, 
+    selection_year text, 
+    disciplines_blocks text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
+
+create table stg.wp_detail (
+    id integer, 
+    discipline_code varchar(20), 
+    title text, 
+    description text, 
+    structural_unit varchar(100), 
+    prerequisites text, 
+    discipline_sections text, 
+    bibliographic_reference text, 
+    outcomes text, 
+    certification_evaluation_tools text, 
+    expertise_status varchar(3),
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
+
+create table stg.practice (
+    id integer, 
+    discipline_code varchar(20), 
+    title text, 
+    year text, 
+    authors text, 
+    op_leader text, 
+    language varchar(20), 
+    qualification text, 
+    kind_of_practice text,
+    type_of_practice text, 
+    way_of_doing_practice text, 
+    format_practice text, 
+    features_content_and_internship text, 
+    features_internship text, 
+    additional_reporting_materials text, 
+    form_of_certification_tools text,
+    passed_great_mark text, 
+    passed_good_mark text, 
+    passed_satisfactorily_mark text, 
+    not_passed_mark text, 
+    evaluation_tools_current_control text,
+    prac_isu_id text, 
+    ze_v_sem varchar(30), 
+    evaluation_tools_v_sem text, 
+    number_of_semesters text, 
+    practice_base varchar(20),  
+    structural_unit varchar(30), 
+    editors text, 
+    bibliographic_reference text, 
+    prerequisites text, 
+    outcomes text,
+    valid_from timestamp DEFAULT NOW(),
+    valid_to timestamp DEFAULT '2999-12-31',
+    is_current boolean DEFAULT TRUE
+);
+
+-- Триггеры
+
+CREATE OR REPLACE FUNCTION scd2_work_programs_trigger()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE stg.work_programs
+    SET valid_to = NOW(), is_current = FALSE
+    WHERE id = OLD.id AND is_current = TRUE;
+
+    INSERT INTO stg.work_programs (id, academic_plan_in_field_of_study, wp_in_academic_plan, valid_from, valid_to, is_current)
+    VALUES (NEW.id, NEW.academic_plan_in_field_of_study, NEW.wp_in_academic_plan, NOW(), '2999-12-31', TRUE);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_update_work_programs
+BEFORE UPDATE ON stg.work_programs
+FOR EACH ROW
+WHEN (OLD.* IS DISTINCT FROM NEW.*)
+EXECUTE FUNCTION scd2_work_programs_trigger();
