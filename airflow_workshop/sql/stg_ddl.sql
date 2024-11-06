50dca5d745fc68b6377a48aecc4ef3e5d330d585
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2024-11-06 21:47:14 +0300

    Удалить неиспользуемые таблицы из stg

--- airflow_workshop/sql/stg_ddl.sql
+++ airflow_workshop/sql/stg_ddl.sql
@@ -44,18 +44,6 @@ create table stg.su_wp (
     is_current boolean DEFAULT TRUE
 );
 
-create table stg.wp_markup (
-    id integer, 
-    title text, 
-    discipline_code integer, 
-    prerequisites text, 
-    outcomes text,
-    valid_from timestamp DEFAULT NOW(),
-    valid_to timestamp DEFAULT '2999-12-31',
-    is_current boolean DEFAULT TRUE
-);
-ALTER TABLE stg.wp_markup ADD CONSTRAINT wp_id_uindex UNIQUE (id);
-
 create table stg.online_courses (
     id integer, 
     institution text, 
@@ -65,25 +53,6 @@ create table stg.online_courses (
     valid_to timestamp DEFAULT '2999-12-31',
     is_current boolean DEFAULT TRUE
 );
-
-create table stg.evaluation_tools (
-    id integer, 
-    type text, 
-    "name" text, 
-    description text, 
-    check_point bool, 
-    deadline integer, 
-    semester integer, 
-    "min" numeric, 
-    "max" numeric, 
-    descipline_sections text,
-    evaluation_criteria text, 
-    wp_id integer,
-    valid_from timestamp DEFAULT NOW(),
-    valid_to timestamp DEFAULT '2999-12-31',
-    is_current boolean DEFAULT TRUE
-);
-ALTER TABLE stg.evaluation_tools ADD CONSTRAINT et_id_uindex UNIQUE (id);
 
 create table stg.disc_by_year (
     id integer, 
@@ -101,49 +70,6 @@ create table stg.up_detail (
     on_check varchar(20), 
     laboriousness integer, 
     academic_plan_in_field_of_study text,
-    valid_from timestamp DEFAULT NOW(),
-    valid_to timestamp DEFAULT '2999-12-31',
-    is_current boolean DEFAULT TRUE
-);
-
-create table stg.up_isu (
-    id integer, 
-    plan_type text, 
-    direction_id text, 
-    ns_id text, 
-    direction_code text, 
-    direction_name text, 
-    edu_program_id text, 
-    edu_program_name text, 
-    faculty_id text, 
-    faculty_name text,
-    training_period text,
-    university_partner text,
-    up_country text, 
-    lang text, 
-    military_department boolean,
-    total_intensity text, 
-    ognp_id text, 
-    ognp_name text, 
-    selection_year text, 
-    disciplines_blocks text,
-    valid_from timestamp DEFAULT NOW(),
-    valid_to timestamp DEFAULT '2999-12-31',
-    is_current boolean DEFAULT TRUE
-);
-
-create table stg.wp_detail (
-    id integer, 
-    discipline_code varchar(20), 
-    title text, 
-    description text, 
-    structural_unit varchar(100), 
-    prerequisites text, 
-    discipline_sections text, 
-    bibliographic_reference text, 
-    outcomes text, 
-    certification_evaluation_tools text, 
-    expertise_status varchar(3),
     valid_from timestamp DEFAULT NOW(),
     valid_to timestamp DEFAULT '2999-12-31',
     is_current boolean DEFAULT TRUE
